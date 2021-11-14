e0cd4383a7e942abf13bad5a83876bbc0c395ebc
Abhishek Dev <abhishek44dev@gmail.com>
2021-11-14 14:24:53 +0530

    Average Subarray (11/11 passed.)
    - The author's solution for the problem
    - Time complexity O(n)

--- average_subarray.py
+++ average_subarray.py
@@ -1,23 +1,22 @@
 # Average Subarray
 
-# This is my version of solution - it's O(n^2)
-# Using Code Snippet: sub_arrays_v1
-def my_average_subarray(arr, N, K):
-    count = 0
-    for i in range(N - (K - 1)):
-        sum = 0
-        for j in range(i, i + K):
-            if arr[j] == "1":
-                sum += 1
-        # Check if average sum of bits is 1,
-        # after inner-loop completes
-        if sum == K:
-            # Flipping last bit in the K subarray
-            arr[(i+K)-1] = "0"
-            i = (i+K) + 1 # Taking a leap forward to (K+1)th position
-            count += 1
-    return count
-
+# A solution from author of the problem - it's just O(n)
+def authors_average_subarray(arr, N, K):
+    sum_bits = ans = 0
+    for current_value in arr:
+        # If current value is 1.
+        if current_value == "1":
+            sum_bits += 1
+            
+            # If sum of bits is equal to K, 
+            # it is same a finding average of sum for K bits.
+            if sum_bits == K:
+                ans += 1
+                sum_bits = 0    # Reset sum_bits
+        else:
+            # Else if current value is not 1.
+            sum_bits = 0 # Reset sum_bits
+    return ans
 
 if __name__ == "__main__":
 
@@ -34,7 +33,7 @@ if __name__ == "__main__":
         for i in range(T):
             N, K = map(int, txt_file.readline().split())
             input_list = txt_file.readline().split()
-            print(my_average_subarray(input_list, N, K))
+            print(authors_average_subarray(input_list, N, K))
     # ----------------------------------------------------------
     """
 
@@ -44,5 +43,5 @@ if __name__ == "__main__":
     for i in range(T):
         N, K = map(int, input().split())
         input_list = input().split()
-        print(my_average_subarray(input_list, N, K))
+        print(authors_average_subarray(input_list, N, K))
     
